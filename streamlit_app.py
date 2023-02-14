9b8b6df80d26204ea6f333a9acf35db84c70ed55
Shang Yongtao <shangyongtao2004@163.com>
2023-02-14 09:16:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,23 +11,52 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# markdown
+st.markdown('Streamlit Demo')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# 设置网页标题
+st.title('一个傻瓜式构建可视化 web的 Python 神器 -- streamlit')
 
-    points_per_turn = total_points / num_turns
+# 展示一级标题
+st.header('1. 安装')
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+st.text('和安装其他包一样，安装 streamlit 非常简单，一条命令即可')
+code1 = '''pip3 install streamlit'''
+st.code(code1, language='bash')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+# 展示一级标题
+st.header('2. 使用')
+
+# 展示二级标题
+st.subheader('2.1 生成 Markdown 文档')
+
+# 纯文本
+st.text('导入 streamlit 后，就可以直接使用 st.markdown() 初始化')
+
+# 展示代码，有高亮效果
+code2 = '''import streamlit as st
+st.markdown('Streamlit Demo')'''
+st.code(code2, language='python')
+
+
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+#     Point = namedtuple('Point', 'x y')
+#     data = []
+
+#     points_per_turn = total_points / num_turns
+
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
+
+#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q'))
