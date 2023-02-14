b0a4585bbfae3eb91d80c1f486e75085ef769440
Shang Yongtao <shangyongtao2004@163.com>
2023-02-14 09:30:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,12 @@ with st.form("my_form"):
    st.write("Inside the form")
    slider_val = st.slider("Form slider")
    checkbox_val = st.checkbox("Form checkbox")
-   title = st.text_input('Movie title', 'Life of Brian')
+   title_val = st.text_input('Movie title', 'Life of Brian')
 
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val，'The current movie title is', title)
+       st.write("slider", slider_val, "checkbox", checkbox_val, 'text_input', title_val)
 
 st.write("Outside the form")
 
